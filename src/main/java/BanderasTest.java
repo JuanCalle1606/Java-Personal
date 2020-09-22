2d56273e7722a5cc160aff63565ef2f5196f87e3
JuanCalle1606 <jpmilojp@gmail.com>
2020-09-22 18:39:22 -0500

    Agregado un programa que usa la clase Bandera

---
+++ src/main/java/BanderasTest.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author Juan Pablo Calle
+ */
+import teepo.Bandera;
+public class BanderasTest{
+	public static void main(String[] args){
+		System.out.println("Bandera de USA:\n");
+		Bandera.USA();
+	}
+}
