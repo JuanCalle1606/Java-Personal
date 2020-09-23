2c48259a6fbd0b0956a67dce0f36738ab340f4bf
JuanCalle1606 <jpmilojp@gmail.com>
2020-09-23 09:49:33 -0500

    Banderas: Agregada la bandera de colombia

--- src/main/java/BanderasTest.java
+++ src/main/java/BanderasTest.java
@@ -7,5 +7,7 @@ public class BanderasTest{
 	public static void main(String[] args){
 		System.out.println("Bandera de USA:\n");
 		Bandera.USA();
+		System.out.println("\nBandera de Colombia:\n");
+		Bandera.Colombia();
 	}
 }
--- src/main/java/teepo/Bandera.java
+++ src/main/java/teepo/Bandera.java
@@ -6,14 +6,21 @@ package teepo;
 public class Bandera{
 	public static void USA(){
 		int flen=13,clen=50;
-		initF:for(int i=0;i<flen;i++){
+		initF:for(int i=0;i<flen;i++)
 			initC:for(int j=0;j<clen;j++)
 				System.out.print(i<7&&j<17?
 				(j==0?ConsoleColors.BLUE_BACKGROUND:"")+(j%2==0?" ":"☆")+
 				(j==16?i%2==0?ConsoleColors.RED_BACKGROUND:ConsoleColors.WHITE_BACKGROUND:""):
 				(j==0?i%2==0?ConsoleColors.RED_BACKGROUND:ConsoleColors.WHITE_BACKGROUND:"")+
-				" "+(j==clen-1?ConsoleColors.RESET:""));
-			System.out.println();
-		}
+				" "+(j==clen-1?ConsoleColors.RESET+"\n":""));
+	}
+	public static void Colombia(){
+		int flen=12,clen=50;
+		initF:for(int i=0;i<flen;i++)
+			initC:for(int j=0;j<clen;j++)
+				System.out.print(j==0?
+				(i<6?ConsoleColors.YELLOW_BACKGROUND:i<9?
+				ConsoleColors.BLUE_BACKGROUND:ConsoleColors.RED_BACKGROUND)+" "
+				:j<clen-1?" ":ConsoleColors.RESET+"\n");
 	}
 }
